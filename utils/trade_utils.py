5c79d31c18f7972c81b87de5cb29b5d5ad852c4b
Young Yang <galahade@sina.com>
2022-06-30 08:05:10 +0800

    Add debug switch contract code.

--- utils/trade_utils.py
+++ utils/trade_utils.py
@@ -22,25 +22,28 @@ def __need_switch_contract(last_symbol, underlying_symbol, ust):
     否则，在交割月之前一个月月初换月。
     '''
     logger = get_logger()
-    last_symbol_list = examine_symbol(last_symbol)
-    today_symbol_list = examine_symbol(underlying_symbol)
-    if not last_symbol_list or not today_symbol_list:
-        logger.warning('新/旧合约代码有误，请检验')
-        return False
-    if today_symbol_list[0] != last_symbol_list[0] or \
-            today_symbol_list[1] != last_symbol_list[1]:
-        logger.warning('新/旧合约品种不一，请检验')
-        return False
-    if underlying_symbol <= last_symbol:
-        logger.warning('新合约非远月合约，不换月')
-        return False
-    last_date = __get_date_from_symbol(last_symbol_list[2])
-    current_date = tafunc.time_to_datetime(ust.quote.datetime)
-    timedelta = last_date - current_date
-    if ust.position.pos != 0 and timedelta.days <= 11:
-        return True
-    elif ust.position.pos == 0 and timedelta.days <= 31:
-        return True
+    if ust.trade_status.ready_s_contract:
+        last_symbol_list = examine_symbol(last_symbol)
+        today_symbol_list = examine_symbol(underlying_symbol)
+        if not last_symbol_list or not today_symbol_list:
+            logger.warning('新/旧合约代码有误，请检验')
+            return False
+        if today_symbol_list[0] != last_symbol_list[0] or \
+                today_symbol_list[1] != last_symbol_list[1]:
+            logger.warning('新/旧合约品种不一，请检验')
+            return False
+        if underlying_symbol <= last_symbol:
+            logger.warning('新合约非远月合约，不换月')
+            return False
+        last_date = __get_date_from_symbol(last_symbol_list[2])
+        current_date = tafunc.time_to_datetime(ust.ticks[-1].datetime)
+        timedelta = last_date - current_date
+        logger.debug(f'原合约{last_symbol},下一个合约{underlying_symbol}'
+                     f'当前时间与原合约交易截止月相差{timedelta.days}天')
+        if ust.position.pos != 0 and timedelta.days <= 11:
+            return True
+        elif ust.position.pos == 0 and timedelta.days <= 31:
+            return True
     return False
 
 
@@ -52,9 +55,7 @@ def switch_contract(ust, api):
     logger = get_logger()
     # 获取最新主力合约
     underlying_symbol = ust.quote.underlying_symbol
-    if ust.trade_status.ready_s_contract \
-       and __need_switch_contract(ust.underlying_symbol, underlying_symbol,
-                                  ust):
+    if __need_switch_contract(ust.underlying_symbol, underlying_symbol, ust):
         new_ust = Underlying_symbol_trade(api, ust.symbol, ust.account, ust.tb)
         last_pos_long = ust.position.pos_long
         quote_time = get_date_str(ust.quote.datetime)
@@ -91,10 +92,13 @@ def wait_to_trade(api, ust):
         # 处理更换主力合约问题
         if api.is_changing(ust.quote, "underlying_symbol"):
             logger.debug(f'{get_date_str(ust.quote.datetime)}平台主力合约已更换,'
+                         f'原合约{ust.underlying_symbol},'
+                         f'新合约{ust.quote.underlying_symbol}'
                          f'开始准备切换合约')
             ust.trade_status.ready_s_contract = True
         if api.is_changing(ust.daily_klines.iloc[-1], "datetime"):
             calc_indicator(ust.daily_klines)
+            logger.debug(f'日线日期:{get_date_str(ust.daily_klines.iloc[-1].datetime)}')
             ust = switch_contract(ust, api)
         if api.is_changing(ust.h2_klines.iloc[-1], "datetime"):
             calc_indicator(ust.h2_klines)
@@ -102,6 +106,5 @@ def wait_to_trade(api, ust):
             calc_indicator(ust.m30_klines)
         if api.is_changing(ust.m5_klines.iloc[-1], "datetime"):
             calc_indicator(ust.m5_klines)
-        # if api.is_changing(ust.ticks):
-        if api.is_changing(ust.quote, "last_price"):
+        if api.is_changing(ust.ticks):
             ust.start_trade()
